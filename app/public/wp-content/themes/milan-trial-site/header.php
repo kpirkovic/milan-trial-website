2ab833bd294a84b6ac15317edaef265de6105bc9
Kristijan Pirkovic <93116779+kpirkovic@users.noreply.github.com>
2022-11-14 07:05:00 +0100

    Setting up WordPress and Adding My Custom Theme
    I've setup WordPress with Local, after that made few changes to the Admin Setting in WP Dashboard.
    
    Created a theme with Bootstrap 5 starter kit, the theme is pretty clean no bloat or any styles, just WP Templates and Clean Bootstrap 5 compiled with Gulp.
    
    After Finishing Those Tasks, I went and created the rough website layout.
    
    Time Spend: 1.5hr

---
+++ app/public/wp-content/themes/milan-trial-site/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<?php wp_head(); ?>
+</head>
+
+<?php
+	$navbar_scheme   = get_theme_mod( 'navbar_scheme', 'navbar-dark bg-primary' ); // Get custom meta-value.
+	$navbar_position = get_theme_mod( 'navbar_position', 'fixed_top' ); // Get custom meta-value.
+
+	$search_enabled  = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
+?>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<a href="#main" class="visually-hidden-focusable"><?php esc_html_e( 'Skip to main content', 'milan-trial-site' ); ?></a>
+
+<div id="wrapper">
+	<header>
+		<nav id="header" class="navbar navbar-expand-md <?php echo esc_attr( $navbar_scheme ); if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom'; endif; if ( is_home() || is_front_page() ) : echo ' home'; endif; ?>">
+			<div class="container d-flex justify-content-between">
+				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php
+						$header_logo = get_theme_mod( 'header_logo' ); // Get custom meta-value.
+
+						if ( ! empty( $header_logo ) ) :
+					?>
+						<img src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+					<?php
+						else :
+							echo esc_attr( get_bloginfo( 'name', 'display' ) );
+						endif;
+					?>
+				</a>
+						
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'milan-trial-site' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div id="navbar" class="collapse navbar-collapse flex-grow-0">
+					<?php
+						// Loading WordPress Custom Menu (theme_location).
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'container'      => '',
+								'menu_class'     => 'navbar-nav d-flex gap-4 align-items-center mt-3 mt-md-0',
+								'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+								'walker'         => new WP_Bootstrap_Navwalker(),
+							)
+						);
+					?>
+				</div><!-- /.navbar-collapse -->
+				<div>
+					<a class="text-white text-decoration-none" href=''><i class="bi bi-facebook">Facebook</i></a>
+					<a class="text-white text-decoration-none" href=''><i class="bi bi-facebook">Instagram</i></a>
+					<a class="text-white text-decoration-none" href=''><i class="bi bi-facebook">Twitter</i></a>
+				</div>
+			</div><!-- /.container -->
+		</nav><!-- /#header -->
+	</header>
+
---
+++ app/public/wp-content/themes/milan-trial-site/home.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Template Name: Home Index
+ * Description: The template for displaying the Blog Home Index Page
+ *
+ */
+get_header(); ?>
+
+<main>
+    <!-- Hero Section -->
+    <section id="hero-section" class='vh-100 bg-primary'>
+        <div class='container py-5 d-flex align-items-center h-100'>
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                            <a class="btn btn-primary btn-lg px-4 me-sm-2" href="#features">Get Started</a>
+                            <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..."></div>
+            </div>
+        </div>
+    </section>
+    <!-- Second Section -->
+    <section class="py-5">
+        <div class="container px-5 my-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="text-center">
+                        <h2 class="fw-bolder">From our blog</h2>
+                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-5">
+                <div class="col-lg-4 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- Third Section -->
+    <section class="py-5">
+        <div class="container px-5 my-5 vh-100">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="text-center">
+                        <h2 class="fw-bolder">From our blog</h2>
+                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row h-50">
+                <div id="carouselExampleControls" class="carousel slide h-100" data-bs-ride="carousel">
+                    <div class="carousel-inner h-100">
+                        <div class="carousel-item active w-100 h-100">
+                        <img src="https://images.unsplash.com/photo-1631183563726-287d84833090?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item w-100 h-100">
+                        <img src="https://images.unsplash.com/photo-1668197819665-e6d3bc7c4f1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item w-100 h-100">
+                        <img src="https://images.unsplash.com/photo-1631183361986-75ab9a581aad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Third Section -->
+    <section class="py-5">
+        <div class="container px-5 my-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="text-center">
+                        <h2 class="fw-bolder">From our blog</h2>
+                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class='vh-100'>
+        <div class='container py-5 d-flex align-items-center h-100'>
+            <div class="row gx-5 align-items-center justify-content-center flex-row-reverse">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder mb-2">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal text-dark-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..."></div>
+            </div>
+        </div>
+    </section>
+    <section class='vh-100'>
+        <div class='container py-5 d-flex align-items-center h-100'>
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder mb-2">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal text-dark-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..."></div>
+            </div>
+        </div>
+    </section>
+    <section class='vh-100'>
+        <div class='container py-5 d-flex align-items-center h-100'>
+            <div class="row gx-5 align-items-center justify-content-center flex-row-reverse">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder mb-2">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal text-dark-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..."></div>
+            </div>
+        </div>
+    </section>
+    <!-- Testimonials Section -->
+    <section class="py-5">
+        <div class="container px-5 my-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="text-center">
+                        <h2 class="fw-bolder">From our blog</h2>
+                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-5">
+                <div class="col-lg-6 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+            </div>
+            <div class="row gx-5">
+                <div class="col-lg-6 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="h-100">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nesciunt alias reprehenderit voluptatibus, ipsum blanditiis.
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- Hero Section -->
+    <section id="hero-section" class='vh-100 bg-primary'>
+        <div class='container py-5 d-flex align-items-center h-100'>
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
+                        <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                            <a class="btn btn-primary btn-lg px-4 me-sm-2" href="#features">Get Started</a>
+                            <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-xxl-6 text-center"><img class="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..."></div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
